3de323b7ef8e88cc8dbce2df88d7ff905b77c635
HAI HUANG <haih@us.ibm.com>
2016-02-11 11:33:52 -0500

    add master port in slave startup script

--- start-mesos-slave.sh
+++ start-mesos-slave.sh
@@ -2,7 +2,8 @@
 
 # Set mesos master ip address here
 MESOS_MASTER_IPADDR=10.143.100.209
+MESOS_MASTER_PORT=5050
 
 # Start mesos slave
 mkdir -p /var/log/mesos
-mesos-slave --master=${MESOS_MASTER_IPADDR} 1>/var/log/mesos/slave.log 2>&1 &
+mesos-slave --master=${MESOS_MASTER_IPADDR}:${MESOS_MASTER_PORT} 1>/var/log/mesos/slave.log 2>&1 &
