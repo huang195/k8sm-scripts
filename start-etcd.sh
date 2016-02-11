81c7faeb0178bf8f5b87dae734bcc6dbe8ae48a5
HAI HUANG <haih@us.ibm.com>
2016-02-11 12:38:49 -0500

    etcd uses container ip

--- start-etcd.sh
+++ start-etcd.sh
@@ -6,5 +6,5 @@ ETCD_IPADDR=10.143.100.209
 # Start etcd
 docker run -d --hostname $(uname -n) --name etcd \
   -p 4001:4001 -p 7001:7001 quay.io/coreos/etcd:v2.0.12 \
-  --listen-client-urls http://${ETCD_IPADDR}:4001 \
+  --listen-client-urls http://0.0.0.0:4001 \
   --advertise-client-urls http://${ETCD_IPADDR}:4001
