e5d80de5db5e99344167de65ca6464daa001f129
HAI HUANG <haih@us.ibm.com>
2016-02-12 16:09:57 -0500

    cosmetic changes

--- start-k8sm.sh
+++ start-k8sm.sh
@@ -10,6 +10,11 @@ cp yaml/podmaster.yaml /etc/kubernetes/manifests/
 cp yaml/controller-mgr.yaml /srv/kubernetes/manifests/
 cp yaml/scheduler.yaml /srv/kubernetes/manifests/
 
+cat <<EOF >mesos-cloud.conf
+[mesos-cloud]
+        mesos-master        = 10.143.100.209
+EOF
+
 kubelet \
   --api_servers=http://127.0.0.1:8080 \
   --register-node=false \
--- stop-k8sm.sh
+++ stop-k8sm.sh
@@ -3,8 +3,8 @@
 # Delete the kubernetes master components
 rm -f /etc/kubernetes/manifests/*
 
-# Wait 15 seconds for kubelet to clean up
-sleep 15
+# Wait 30 seconds for kubelet to clean up
+sleep 30
 
 # Finally kill the kubelet itself
 killall -9 kubelet
