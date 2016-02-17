99085cb08d51ee0e4a69b6bddaf4a05dcc94f844
HAI HUANG <haih@us.ibm.com>
2016-02-17 10:59:27 -0500

    forgot to add -a to docker when rm

--- stop-k8sm.sh
+++ stop-k8sm.sh
@@ -9,4 +9,4 @@ rm -f /etc/kubernetes/manifests/*
 # Remove containers started by k8s
 docker stop `docker ps | grep k8s_ | awk '{print $1}'`
 sleep 1
-docker rm `docker ps | grep k8s_ | awk '{print $1}'`
+docker rm `docker ps -a | grep k8s_ | awk '{print $1}'`
