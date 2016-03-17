b66f7e3f13e5c40d6d3d793c159e695321d5abad
HAI HUANG <haih@us.ibm.com>
2016-03-17 14:18:01 -0400

    adding stock abac policy to etcd

--- start-remoteabac.sh
+++ start-remoteabac.sh
@@ -4,12 +4,20 @@
 
 #remoteabac --address=${RABAC_IP}:${RABAC_PORT} --tls-cert-file=/var/run/kubernetes/apiserver.pem --tls-private-key-file=/var/run/kubernetes/apiserver-key.pem --authorization-policy-file=/etc/kubernetes/k8sm-auth-policy 1> abac.log 2>&1 &
 
-docker run -p ${RABAC_PORT}:{$RABAC_PORT} \
-	-v /var/run/kubernetes/:/var/run/kubernetes \
-	haih/remoteabac \
-	-name remoteabac \
-	--address=:8888 \
-	--tls-cert-file=/var/run/kubernetes/apiserver.pem \
-	--tls-private-key-file=/var/run/kubernetes/apiserver-key.pem \
-	--authorization-policy-file=etcd@http://10.143.100.209:4001/abac-policy
+docker exec etcd /etcdctl set /abac-policy '{"apiVersion": "abac.authorization.kubernetes.io/v1beta1", "kind": "Policy", "spec": {"user":"*", "nonResourcePath": "*", "readonly": true}}
+{"apiVersion": "abac.authorization.kubernetes.io/v1beta1", "kind": "Policy", "spec": {"user": "admin", "namespace": "*", "resource": "*", "apiGroup": "*"}}
+{"apiVersion": "abac.authorization.kubernetes.io/v1beta1", "kind": "Policy", "spec": {"user": "alice", "namespace": "alice", "resource": "*", "apiGroup": "*"}}
+{"apiVersion": "abac.authorization.kubernetes.io/v1beta1", "kind": "Policy", "spec": {"user": "kubernetes-scheduler", "namespace": "*", "resource": "*", "apiGroup": "*"}}
+{"apiVersion": "abac.authorization.kubernetes.io/v1beta1", "kind": "Policy", "spec": {"user": "kubernetes-controller-mgr", "namespace": "*", "resource": "*", "apiGroup": "*"}}
+{"apiVersion": "abac.authorization.kubernetes.io/v1beta1", "kind": "Policy", "spec": {"user": "kubelet", "namespace": "*", "resource": "*", "apiGroup": "*"}}
+{"apiVersion": "abac.authorization.kubernetes.io/v1beta1", "kind": "Policy", "spec": {"user": "proxy", "namespace": "*", "resource": "*", "apiGroup": "*"}}'
 
+docker run -p ${RABAC_PORT}:${RABAC_PORT} \
+        -v /var/run/kubernetes/:/var/run/kubernetes \
+        --name remoteabac \
+        -d \
+        haih/remoteabac \
+        --address=:8888 \
+        --tls-cert-file=/var/run/kubernetes/apiserver.pem \
+        --tls-private-key-file=/var/run/kubernetes/apiserver-key.pem \
+        --authorization-policy-file=etcd@http://${ETCD_IP}:${ETCD_PORT}/abac-policy
