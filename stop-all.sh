14796a33288f28f7def8717fe461e45bc106104a
Hai Huang <haih@us.ibm.com>
2016-07-31 10:43:30 -0400

    minor fixes

--- stop-all.sh
+++ stop-all.sh
@@ -2,8 +2,11 @@ service kubelet stop
 docker kill `docker ps -q`
 docker rm `docker ps -aq`
 service docker stop
+rm -rf /openstack/docker/*
 service docker start
 killall -9 ucarp
-ip addr del 10.140.146.3/26 dev eth0
 rm -rf /var/etcd/data/*
+rm -rf /etc/kubernetes
+rm -rf /srv/kubernetes
 
+ip addr del 10.140.146.0/26 dev eth0
