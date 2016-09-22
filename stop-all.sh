6e59564cd60c6db79835cf944362876e72e28c72
Hai Huang <haih@us.ibm.com>
2016-09-22 10:59:13 -0400

    add pod test case

--- stop-all.sh
+++ stop-all.sh
@@ -3,6 +3,7 @@ docker kill `docker ps -q`
 docker rm `docker ps -aq`
 service docker stop
 rm -rf /openstack/docker/*
+rm -rf /var/tmp/*
 service docker start
 killall -9 ucarp
 rm -rf /var/etcd/data/*
